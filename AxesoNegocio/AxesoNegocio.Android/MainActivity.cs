b86620adad3d8d9a0a8743e5762eb36b03fadba4
alexis vidal <alexis.vidal@outlook.com>
2020-07-18 07:21:45 -0500

    Second

--- AxesoNegocio/AxesoNegocio.Android/MainActivity.cs
+++ AxesoNegocio/AxesoNegocio.Android/MainActivity.cs
@@ -6,28 +6,68 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android;
 
 namespace AxesoNegocio.Droid
 {
-    [Activity(Label = "AxesoNegocio", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AxesoNegocio", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme",
+        MainLauncher = false,
+        ConfigurationChanges = ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjg4NzAwQDMxMzgyZTMyMmUzMEd1bnZ1dDBKcXJMUWJVdVdacm9mbmpLaHlCd3VzSzFROUVJKytQNVowbm89");
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
             LoadApplication(new App());
+            if ((CheckSelfPermission(Manifest.Permission.AccessCoarseLocation) != (int)Permission.Granted))
+            {
+                RequestPermissions(new string[] { Manifest.Permission.AccessCoarseLocation }, 0);
+            }
+            if ((CheckSelfPermission(Manifest.Permission.AccessFineLocation) != (int)Permission.Granted))
+            {
+                RequestPermissions(new string[] { Manifest.Permission.AccessFineLocation }, 0);
+            }
+            if ((CheckSelfPermission(Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted))
+            {
+                RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, 0);
+            }
+            if ((CheckSelfPermission(Manifest.Permission.ReadExternalStorage) != (int)Permission.Granted))
+            {
+                RequestPermissions(new string[] { Manifest.Permission.ReadExternalStorage }, 0);
+            }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+            if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
+            {
+                // Do something if there are some pages in the `PopupStack`
+            }
+            else
+            {
+                // Do something if there are not any pages in the `PopupStack`
+            }
         }
     }
 }
---
+++ AxesoNegocio/AxesoNegocio.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AxesoNegocio.Droid
+{
+    [Activity(Label = "Axeso Negocio",
+        Icon = "@mipmap/icon",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        Theme = "@style/Theme.SplashE",
+        MainLauncher = true,
+        ScreenOrientation = ScreenOrientation.Portrait,
+        NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
